340d653ed9ff6926d4c961aaf94f7d6f1379dea0
HenriqueGMen <henrimeen24@gmail.com>
2024-03-20 11:47:52 -0300

    working with transform and writable stream

--- Chapter I/01/stream/fundamental.js
+++ Chapter I/01/stream/fundamental.js
@@ -1,13 +1,13 @@
-import { Readable } from 'node:stream'
+import { Readable, Transform, Writable } from 'node:stream'
 
-class oneToHundred extends Readable {
+class OneToHundred extends Readable {
 index = 1
   //todo stream readable tem um metodo read
   _read() {
     const i = this.index++
 
     setTimeout(() => {
-      if (i > 100) {
+      if (i > 1000) {
         this.push(null)
       } else {
         const buf = Buffer.from(String(i))
@@ -18,4 +18,21 @@ index = 1
   }
 }
 
-new oneToHundred().pipe(process.stdout)
+class InvertNumbers extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
+new OneToHundred()
+  .pipe(new InvertNumbers())
+  .pipe(new MultiplyByTenStream())
